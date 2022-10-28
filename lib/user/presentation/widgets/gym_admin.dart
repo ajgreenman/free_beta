836058c04f8752fe08666c87129d8595c3a28294
Andrew Greenman <agreenman@hagerty.com>
2022-10-28 15:59:06 -0400

    wireframe for edit refresh screen

--- lib/user/presentation/widgets/gym_admin.dart
+++ lib/user/presentation/widgets/gym_admin.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:free_beta/app/presentation/widgets/info_card.dart';
 import 'package:free_beta/app/theme.dart';
 import 'package:free_beta/gym/presentation/create_route_screen.dart';
+import 'package:free_beta/gym/presentation/edit_refresh_schedule.dart';
 
 class GymAdmin extends StatelessWidget {
   const GymAdmin({Key? key}) : super(key: key);
@@ -18,12 +19,44 @@ class GymAdmin extends StatelessWidget {
             SizedBox(height: FreeBetaSizes.l),
             _CreateRouteButton(),
             SizedBox(height: FreeBetaSizes.l),
+            _AddRefreshSchedule(),
+            SizedBox(height: FreeBetaSizes.l),
             Text(
               'To edit a route, first go to the route in the Routes tab. Then, tap the pencil icon in the top right corner of the screen.',
             ),
           ],
         ),
       );
+}
+
+class _AddRefreshSchedule extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => Navigator.of(context).push(EditRefreshSchedule.route()),
+      child: Padding(
+        padding: FreeBetaPadding.xlHorizontal,
+        child: Text(
+          'Add new route refresh',
+          style: FreeBetaTextStyle.h4.copyWith(
+            color: FreeBetaColors.white,
+          ),
+        ),
+      ),
+      style: ButtonStyle(
+        side: MaterialStateProperty.all(
+          BorderSide(
+            width: 2,
+          ),
+        ),
+        padding: MaterialStateProperty.all(
+          const EdgeInsets.symmetric(
+            vertical: FreeBetaSizes.ml,
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class _CreateRouteButton extends StatelessWidget {
@@ -38,7 +71,7 @@ class _CreateRouteButton extends StatelessWidget {
       child: Padding(
         padding: FreeBetaPadding.xlHorizontal,
         child: Text(
-          'Create Route',
+          'Create route',
           style: FreeBetaTextStyle.h4.copyWith(
             color: FreeBetaColors.white,
           ),
