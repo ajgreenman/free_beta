17557f76fe75099e74e702eccbb3365eef9347b0
Andrew Greenman <a.j.greenman@gmail.com>
2023-03-31 13:45:23 -0400

    class providers

--- lib/class/infrastructure/class_providers.dart
+++ lib/class/infrastructure/class_providers.dart
@@ -1,32 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:free_beta/app/infrastructure/app_providers.dart';
 import 'package:free_beta/class/infrastructure/class_api.dart';
 import 'package:free_beta/class/infrastructure/class_remote_data_provider.dart';
 import 'package:free_beta/class/infrastructure/class_repository.dart';
 import 'package:free_beta/class/infrastructure/models/class_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final classApiProvider = Provider(
-  (ref) => ClassApi(
+part 'class_providers.g.dart';
+
+@riverpod
+ClassApi classApi(ClassApiRef ref) {
+  return ClassApi(
     classRepository: ref.watch(classRepositoryProvider),
-  ),
-);
+  );
+}
 
-final classRepositoryProvider = Provider(
-  (ref) => ClassRepository(
+@riverpod
+ClassRepository classRepository(ClassRepositoryRef ref) {
+  return ClassRepository(
     classRemoteDataProvider: ref.watch(classRemoteDataProvider),
-  ),
-);
+  );
+}
 
-final classRemoteDataProvider = Provider(
-  (ref) => ClassRemoteDataProvider(
+@riverpod
+ClassRemoteDataProvider classRemoteData(
+  ClassRemoteDataRef ref,
+) {
+  return ClassRemoteDataProvider(
     firebaseFirestore: FirebaseFirestore.instance,
     crashlyticsApi: ref.read(crashlyticsApiProvider),
-  ),
-);
+  );
+}
 
-final fetchClassesProvider = FutureProvider<List<ClassModel>>((ref) async {
-  final routeApi = ref.watch(classApiProvider);
+@riverpod
+Future<List<ClassModel>> fetchClasses(FetchClassesRef ref) async {
+  final classApi = ref.watch(classApiProvider);
 
-  return routeApi.getClassSchedule();
-});
+  return classApi.getClassSchedule();
+}
---
+++ lib/class/infrastructure/class_providers.g.dart
@@ -0,0 +1,69 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'class_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$classApiHash() => r'b51c682667dd1740992813889399e7a549c26260';
+
+/// See also [classApi].
+@ProviderFor(classApi)
+final classApiProvider = AutoDisposeProvider<ClassApi>.internal(
+  classApi,
+  name: r'classApiProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$classApiHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ClassApiRef = AutoDisposeProviderRef<ClassApi>;
+String _$classRepositoryHash() => r'f3a4a0aa1728c0b39e23f4c790ab9b165033098c';
+
+/// See also [classRepository].
+@ProviderFor(classRepository)
+final classRepositoryProvider = AutoDisposeProvider<ClassRepository>.internal(
+  classRepository,
+  name: r'classRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$classRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ClassRepositoryRef = AutoDisposeProviderRef<ClassRepository>;
+String _$classRemoteDataHash() => r'e5ee7c48243ec39381b3e50bfaff3b62b4293a15';
+
+/// See also [classRemoteData].
+@ProviderFor(classRemoteData)
+final classRemoteDataProvider =
+    AutoDisposeProvider<ClassRemoteDataProvider>.internal(
+  classRemoteData,
+  name: r'classRemoteDataProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$classRemoteDataHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ClassRemoteDataRef = AutoDisposeProviderRef<ClassRemoteDataProvider>;
+String _$fetchClassesHash() => r'3cd708e72ffbb05cc1d3b8c60922ffbef19c0fee';
+
+/// See also [fetchClasses].
+@ProviderFor(fetchClasses)
+final fetchClassesProvider =
+    AutoDisposeFutureProvider<List<ClassModel>>.internal(
+  fetchClasses,
+  name: r'fetchClassesProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$fetchClassesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FetchClassesRef = AutoDisposeFutureProviderRef<List<ClassModel>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
